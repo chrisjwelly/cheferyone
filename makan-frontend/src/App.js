710fbf5fe25abbb7c8d97ba1764feba60f40a3b2
nelsontky <nelson@ccb.wtf>
2020-09-10 23:49:42 +0800

    Allow api data to be cached

--- makan-frontend/src/App.js
+++ makan-frontend/src/App.js
@@ -1,16 +1,15 @@
 import React from "react";
 
-import { useGet } from "./utils/restU-utils";
+import { useGet } from "./utils/rest-utils";
 
 function App() {
-  const { data, isLoading, error } = useGet(
-    "https://jsonplaceholder.typicode.com/todos/1"
-  );
+  const { data, isLoading, error } = useGet("/api/test/index");
+
   return (
     <div>
       <h1>Hello Home Cooks!</h1>
       <p>Example loading from rails backend</p>
-      {isLoading ? <p>Loading...</p> : <p>{data.title}</p>}
+      {isLoading || error ? <p>Loading...</p> : <p>{data.content}</p>}
     </div>
   );
 }
