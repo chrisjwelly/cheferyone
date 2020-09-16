42b89a424433797d1d325c95ea64c763519d6bb5
Yehezkiel01 <yehezkieltheodoroes@yahoo.co.id>
2020-09-17 04:26:50 +0800

    Configure logo + seed datas

--- makan/app/models/restaurant.rb
+++ makan/app/models/restaurant.rb
@@ -1,4 +1,12 @@
 class Restaurant < ApplicationRecord
+  include Rails.application.routes.url_helpers
+
+  has_one_attached :logo
+
   belongs_to :user, touch: true
   has_many :menus, dependent: :destroy
+
+  def get_logo_url
+    url_for(self.logo)
+  end
 end
--- makan/db/seeds.rb
+++ makan/db/seeds.rb
@@ -11,10 +11,30 @@
   User.create(email: "user#{i + 1}@example.com", password: "123456", username: "user#{i+1}", authentication_token: "user#{i + 1}")
 end
 
+def get_logos_path(filename)
+  Rails.root.join('app', 'assets', 'restaurant_logos', filename)
+end
+
+def get_image_content_type(filename)
+  "image/#{filename.split('.').last}"
+end
+
+def get_file(filename)
+  File.open(get_logos_path(filename))
+end
+
+restaurant_logos = ["DinTaiFung.png", "KFC.png", "McDonalds.png", "PizzaHut.png", "Subway.png"]
 # Create Home Chefs, Restaurants and Menus
 5.times do |i|
   chef = User.create(email: "chef#{i + 1}@example.com", password: "123456", username: "chef#{i+1}", authentication_token: "chef#{i + 1}")
   restaurant = chef.create_restaurant(description: "Chef's Kitchen #{i + 1}", location: "NUS PGPR Block #{i + 1}")
+
+  filename = restaurant_logos[i]
+  restaurant.logo.attach({
+    io: get_file(filename),
+    filename: filename,
+    content_type: get_image_content_type(filename),
+  })
 
   3.times do |j|
     restaurant.menus.create(name: "Menu ##{j + 1}", description: "A great menu.", price: 0.5 + i + j, rating: i + 0.5)
