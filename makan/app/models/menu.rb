69156c5acdb1067abff828dd9a7bb7cf92dcccb3
Yehezkiel01 <yehezkieltheodoroes@yahoo.co.id>
2020-09-17 04:45:53 +0800

    Add menus image

--- makan/app/models/menu.rb
+++ makan/app/models/menu.rb
@@ -1,3 +1,18 @@
 class Menu < ApplicationRecord
+  include Rails.application.routes.url_helpers
+
   belongs_to :restaurant, touch: true
+
+  has_one_attached :image
+
+  def get_image_url
+    url_for(self.image)
+  end
+
+  # Append logo to JSON
+  def as_json(options)
+    super(options).merge({
+      "image_url" => get_image_url
+    })
+  end
 end
