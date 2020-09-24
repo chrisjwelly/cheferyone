6274b9b09022a266dbdc23962b6adabcb1987235
Yehezkiel01 <yehezkieltheodoroes@yahoo.co.id>
2020-09-25 06:45:46 +0800

    Send extra datas

--- app/jobs/notification_relay_job.rb
+++ app/jobs/notification_relay_job.rb
@@ -2,6 +2,12 @@ class NotificationRelayJob < ApplicationJob
   queue_as :default
 
   def perform(notification)
-    ActionCable.server.broadcast "notifications:#{notification.recipient_id}", {content: notification.content, object: notification.notifiable, to: notification.recipient}
+    ActionCable.server.broadcast "notifications:#{notification.recipient_id}", {
+      content: notification.content,
+      object: notification.notifiable,
+      to: notification.recipient,
+      image_url: image_url,
+      redirect_url: redirect_url
+    }
   end
 end
