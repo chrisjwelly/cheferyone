4ab678949f2b8d82347144a17193d1dddfeb028f
Yehezkiel01 <yehezkieltheodoroes@yahoo.co.id>
2020-09-25 07:10:26 +0800

    Fix notification json

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,7 +1,19 @@
 class Notification < ApplicationRecord
-	belongs_to :recipient, class_name: "User"
-	belongs_to :notifiable, polymorphic: true
+  default_scope { order('created_at DESC') }
 
-	after_commit -> { NotificationRelayJob.perform_later(self) }
-	scope :unread, -> {where(read_at: nil)}
+  belongs_to :recipient, class_name: "User"
+  belongs_to :notifiable, polymorphic: true
+
+  after_commit -> { NotificationRelayJob.perform_later(self) }
+  scope :unread, -> {where(read_at: nil)}
+
+  def as_json(options = {})
+    super(options).merge({
+      "object" => {
+        image_url: image_url,
+        redirect_url: redirect_url,
+        created_at: created_at
+      }
+    })
+  end
 end
